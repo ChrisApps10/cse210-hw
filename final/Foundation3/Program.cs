8509e7dd41ea2578e546da362f85a6d420006d6f
Chris Aparicio <chrisapps10@byui.edu>
2023-04-03 20:24:02 -0700

    Finished Foundation 4

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Console.WriteLine("---------------------------------------------------------");
+        Address address1 = new Address(" ");
+        Reception reception1 = new Reception(" ", " "," ", " ", address1, " ");
+        reception1.AddEmails(new string[] { " ", " ", " " });
+        reception1.DisplayShortDeets();
+        reception1.DisplayStandardDeets();
+        reception1.DisplayFullDeets();
+        Console.WriteLine("---------------------------------------------------------");
+
+        Address address2 = new Address(" ");
+        Lecture lecture1 = new Lecture(" ", ," ", " "," ", " ", address2, " ");
+        lecture1.DisplayShortDeets();
+        lecture1.DisplayStandardDeets();
+        lecture1.DisplayFullDeets();
+        Console.WriteLine("---------------------------------------------------------");
+
+        Address address3 = new Address(" ");
+        Gathering gathering1 = new Gathering(" ", " ", " "," ", " ", address3, " ");
+        gathering1.DisplayShortDeets();
+        gathering1.DisplayStandardDeets();
+        gathering1.DisplayFullDeets();
+        Console.WriteLine("---------------------------------------------------------");
     }
 }
