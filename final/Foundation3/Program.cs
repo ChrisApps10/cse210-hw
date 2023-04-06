da34daa6e61eba3f3e45de397c8e0cdd577753e4
Chris Aparicio <chrisapps10@byui.edu>
2023-04-05 21:10:22 -0700

    Fixed Foundation 3

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,27 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("---------------------------------------------------------");
-        Address address1 = new Address(" ");
-        Reception reception1 = new Reception(" ", " "," ", " ", address1, " ");
-        reception1.AddEmails(new string[] { " ", " ", " " });
-        reception1.DisplayShortDeets();
-        reception1.DisplayStandardDeets();
-        reception1.DisplayFullDeets();
-        Console.WriteLine("---------------------------------------------------------");
+        Console.WriteLine("");
+        Address _address1 = new Address("525 S Center St, Rexburg, ID 83460");
+        Lecture _lecture = new Lecture("Brother Christopher Pitts", 150, "Christopher Pitts's Lecture", "Come join us as Professor Pitts's instructs us in the C# coding language", "12/10/2023", "7:00am", _address1, "Lecture");
+        _lecture.DisplayStandardDetails();
+        _lecture.DisplayFullDetails();
+        _lecture.DisplayShortDetails();
+        Console.WriteLine("");
+        
+        Address _address2 = new Address("");
+        Reception _reception = new Reception("Emily and Steve's Wedding Reception", "Come celebrate with the newly wedded couple, Emily and Steve!", "5/25/2024", "8:00pm", _address2, "Reception");
+        _reception.AddEmails(new string[] { "stevepaker90@gmail.com", "jane.tucker@hotmail.com", "juangarcia25@gmail.com"});
+        _reception.DisplayStandardDetails();
+        _reception.DisplayFullDetails();
+        _reception.DisplayShortDetails();
+        Console.WriteLine("");
 
-        Address address2 = new Address(" ");
-        Lecture lecture1 = new Lecture(" ", ," ", " "," ", " ", address2, " ");
-        lecture1.DisplayShortDeets();
-        lecture1.DisplayStandardDeets();
-        lecture1.DisplayFullDeets();
-        Console.WriteLine("---------------------------------------------------------");
-
-        Address address3 = new Address(" ");
-        Gathering gathering1 = new Gathering(" ", " ", " "," ", " ", address3, " ");
-        gathering1.DisplayShortDeets();
-        gathering1.DisplayStandardDeets();
-        gathering1.DisplayFullDeets();
-        Console.WriteLine("---------------------------------------------------------");
+        Address _address3 = new Address("18 Howe Ave, Passaic, NJ 07055");
+        Gathering _gathering = new Gathering("Light Rain", "Return Missionary Reunion", "Return Missionary Reunion for all Missionaries that served in Passaic from 2019 to 2021", "7/27/2025", "5:00pm", _address3, "Gathering");
+        _gathering.DisplayStandardDetails();
+        _gathering.DisplayFullDetails();
+        _gathering.DisplayShortDetails();
+        Console.WriteLine("");
     }
 }
